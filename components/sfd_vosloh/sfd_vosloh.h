92211900752340218b0a9dc337ca47d0d67f8484
Christian Backus <chrissileinus@googlemail.com>
2023-08-28 13:59:27 +0200

    gfjhfgjh

--- components/sfd_vosloh/sfd_vosloh.h
+++ components/sfd_vosloh/sfd_vosloh.h
@@ -18,38 +18,24 @@ namespace esphome
       int cursor_position = 127;
       int last_char_position = 127;
       
-      bool cursor_increment()
+      bool _cursor_increment()
       {
         if (this->cursor_position < this->MAX_ADDRESS)
         {
           this->cursor_position++;
           return true;
         }
-        ESP_LOGD(TAG, "[cursor_increment]: reached MAX_ADDRESS");
+        ESP_LOGD(TAG, "[_cursor_increment]: reached MAX_ADDRESS");
         return false;
       }
 
-      void cursor_reset()
+      void _cursor_reset()
       {
         this->cursor_position = 0;
         this->last_char_position = 0;
       }
 
-      bool next_line()
-      {
-        int new_position = this->line_length * (1 + ceil(this->cursor_position / this->line_length));
-        if (new_position < this->MAX_ADDRESS)
-        {
-          this->cursor_position = new_position;
-          ESP_LOGD(TAG, "[next_line] cursor position: %3d", this->cursor_position);
-          return true;
-        }
-        ESP_LOGD(TAG, "[next_line]: reached MAX_ADDRESS");
-        return false;
-      }
-
-    public:
-      bool write_char_at(char c, int pos)
+      bool _write_char_at(char c, int pos)
       {
         if (pos <= this->MAX_ADDRESS)
         {
@@ -59,30 +45,44 @@ namespace esphome
           delay(5);
           return true;
         }
-        ESP_LOGD(TAG, "[write_char_at]: reached MAX_ADDRESS");
+        ESP_LOGD(TAG, "[_write_char_at]: reached MAX_ADDRESS");
         return false;
       }
 
-      bool write_char(char c)
+      bool _write_char(char c)
       {
-        if (!this->write_char_at(c, this->cursor_position + 1))
+        if (!this->_write_char_at(c, this->cursor_position + 1))
           return false;
-        if (!this->cursor_increment())
+        if (!this->_cursor_increment())
           return false;
         this->last_char_position = (this->last_char_position < this->cursor_position) ? this->cursor_position : this->last_char_position;
         return true;
       }
 
-      void write_string(std::string str)
+      void _write_string(std::string str)
       {
-        ESP_LOGD(TAG, "[write_string] at pos: %3d; string: \"%s\"", this->cursor_position, str.c_str());
+        ESP_LOGD(TAG, "[_write_string] at pos: %3d; string: \"%s\"", this->cursor_position, str.c_str());
         for (int i = 0; i < str.length(); i++)
         {
-          if (!this->write_char(str[i]))
+          if (!this->_write_char(str[i]))
             break;
         }
       }
 
+      bool _next_line()
+      {
+        int new_position = this->line_length * (1 + ceil(this->cursor_position / this->line_length));
+        if (new_position < this->MAX_ADDRESS)
+        {
+          this->cursor_position = new_position;
+          ESP_LOGD(TAG, "[_next_line] cursor position: %3d", this->cursor_position);
+          return true;
+        }
+        ESP_LOGD(TAG, "[_next_line]: reached MAX_ADDRESS");
+        return false;
+      }
+
+    public:
       void set_line_length(uint8_t length) { this->line_length = length; }
 
       void setup() override
@@ -102,7 +102,7 @@ namespace esphome
       void roll()
       {
         this->write_byte(0x82);
-        this->cursor_reset();
+        this->_cursor_reset();
       }
 
       void adapt()
@@ -114,14 +114,21 @@ namespace esphome
       {
         for (int i = 0; i < last_char_position; i++)
         {
-          this->write_char_at(' ', i + 1);
+          this->_write_char_at(' ', i + 1);
         }
-        this->cursor_reset();
+        this->_cursor_reset();
       }
 
       void blank()
       {
         this->clear();
+        this->adapt();
+      }
+
+      void write_string(std::string str)
+      {
+        this->clear();
+        this->_write_string(str);
         this->adapt();
       }
 
@@ -142,9 +149,12 @@ namespace esphome
 
           int rest_of_line = this->line_length - (this->cursor_position % this->line_length);
 
-          if (rest_of_line < word.length() && !this->next_line())
+          if (rest_of_line < word.length())
           {
-            break;
+            if (!this->_next_line())
+            {
+              break;
+            }
           }
 
           bool nl = false;
@@ -152,7 +162,7 @@ namespace esphome
           {
             if (pos == 0)
             {
-              this->next_line();
+              this->_next_line();
               nl = false;
             }
             else
@@ -160,12 +170,12 @@ namespace esphome
             word.erase(pos, 1);
           }
 
-          this->write_string(word);
+          this->_write_string(word);
 
           if (nl)
-            this->next_line();
+            this->_next_line();
           else
-            this->cursor_increment();
+            this->_cursor_increment();
         }
 
         this->adapt();
@@ -176,7 +186,7 @@ namespace esphome
         this->clear();
         for (int i = 0; i < this->MAX_ADDRESS; i++)
         {
-          this->write_char(48 + ((i + 1) % 10));
+          this->_write_char(48 + ((i + 1) % 10));
         }
         this->adapt();
       }
