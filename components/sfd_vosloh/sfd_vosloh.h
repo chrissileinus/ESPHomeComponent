45eac4e0bff2570e6c0161b5b9d31f8846630049
Christian Backus <chrissileinus@googlemail.com>
2023-08-31 15:09:24 +0200

    Update sfd_vosloh.h

--- components/sfd_vosloh/sfd_vosloh.h
+++ components/sfd_vosloh/sfd_vosloh.h
@@ -1,196 +1,81 @@
+#pragma once
+
 #include "esphome.h"
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome
 {
   namespace sfd_vosloh
   {
+    static const char *TAG = "sfd_vosloh";
 
-    static const char *TAG = "sfd_vosloh.component";
+    static const uint8_t RAW = 0b00000000;
+    static const uint8_t ALIGN_LEFT = 0b10000000;
+    static const uint8_t ALIGN_CENTER = 0b01000000;
+    static const uint8_t ALIGN_RIGHT = 0b00100000;
+    static const uint8_t WORD_WRAP = 0b00010000;
+    static const uint8_t OVERWRITE = 0b00001000;
 
-    class sfdVosloh : public Component, public uart::UARTDevice
+    class sfdVosloh : public Component, public uart::UARTDevice, public text_sensor::TextSensor, public sensor::Sensor
     {
     protected:
-      const int MAX_ADDRESS = 127;
+      const uint8_t POSITION_MAX = 127;
+      const uint8_t UART_TIMEOUT = 10;
 
-      int line_length = this->MAX_ADDRESS;
-      int cursor_position = 127;
-      int last_char_position = 127;
-      
-      bool _cursor_increment()
+      const uint8_t _WRITE = 0x88;
+      const uint8_t _READ = 0x85;
+      const uint8_t _STATE = 0x84;
+      const uint8_t _ADAPT = 0x81;
+      const uint8_t _ROLL = 0x82;
+
+      int row_length = this->POSITION_MAX;
+      int current_position = 1;
+
+      text_sensor::TextSensor *current_content;
+      sensor::Sensor *last_module;
+
+      bool set_string(std::string string, uint8_t position);
+      bool set_character(char character, uint8_t position);
+
+      char get_character(uint8_t position);
+      uint8_t get_state(uint8_t position);
+      uint8_t collect_respond();
+
+      void update_last_module();
+      void update_current_content();
+      void update_current_state();
+
+      // helper
+
+      // _erase_all
+      //  find character in string and erase them all.
+      static void _erase_all(std::string &string, char character)
       {
-        if (this->cursor_position < this->MAX_ADDRESS)
+        size_t pos = 0;
+        while ((pos = string.find('\n')) != std::string::npos)
         {
-          this->cursor_position++;
-          return true;
+          string.erase(pos, 1);
         }
-        ESP_LOGD(TAG, "[_cursor_increment]: reached MAX_ADDRESS");
-        return false;
-      }
-
-      void _cursor_reset()
-      {
-        this->cursor_position = 0;
-        this->last_char_position = 0;
-      }
-
-      bool _write_char_at(char c, int pos)
-      {
-        if (pos <= this->MAX_ADDRESS)
-        {
-          this->write_byte(0x88);
-          this->write_byte(pos);
-          this->write_byte(c);
-          delay(5);
-          return true;
-        }
-        ESP_LOGD(TAG, "[_write_char_at]: reached MAX_ADDRESS");
-        return false;
-      }
-
-      bool _write_char(char c)
-      {
-        if (!this->_write_char_at(c, this->cursor_position + 1))
-          return false;
-        if (!this->_cursor_increment())
-          return false;
-        this->last_char_position = (this->last_char_position < this->cursor_position) ? this->cursor_position : this->last_char_position;
-        return true;
-      }
-
-      void _write_string(std::string str)
-      {
-        ESP_LOGD(TAG, "[_write_string] at pos: %3d; string: \"%s\"", this->cursor_position, str.c_str());
-        for (int i = 0; i < str.length(); i++)
-        {
-          if (!this->_write_char(str[i]))
-            break;
-        }
-      }
-
-      bool _next_line()
-      {
-        int new_position = this->line_length * (1 + ceil(this->cursor_position / this->line_length));
-        if (new_position < this->MAX_ADDRESS)
-        {
-          this->cursor_position = new_position;
-          ESP_LOGD(TAG, "[_next_line] cursor position: %3d", this->cursor_position);
-          return true;
-        }
-        ESP_LOGD(TAG, "[_next_line]: reached MAX_ADDRESS");
-        return false;
       }
 
     public:
-      void set_line_length(uint8_t length) { this->line_length = length; }
+      void setup_row_length(uint8_t length) { this->row_length = length; }
+      void setup_current_content(text_sensor::TextSensor *sensor) { this->current_content = sensor; }
+      void setup_last_module(sensor::Sensor *sensor) { this->last_module = sensor; }
 
-      void setup() override
-      {
-        this->roll();
-      }
+      void setup() override;
+      void dump_config() override;
+      void loop() override;
 
-      void dump_config() override
-      {
-        ESP_LOGCONFIG(TAG, "line length: %3d", this->line_length);
-      }
+      void roll();
+      void clear(bool adapt = true);
 
-      void loop() override
-      {
-      }
+      void set_content(std::string str, uint8_t mode = 0x00, uint8_t row = 1);
 
-      void roll()
-      {
-        this->write_byte(0x82);
-        this->_cursor_reset();
-      }
-
-      void adapt()
-      {
-        this->write_byte(0x81);
-      }
-
-      void clear()
-      {
-        for (int i = 0; i < last_char_position; i++)
-        {
-          this->_write_char_at(' ', i + 1);
-        }
-        this->_cursor_reset();
-      }
-
-      void blank()
-      {
-        this->clear();
-        this->adapt();
-      }
-
-      void write_string(std::string str)
-      {
-        this->clear();
-        this->_write_string(str);
-        this->adapt();
-      }
-
-      void write_text(std::string str)
-      {
-        ESP_LOGD(TAG, "write_text: %s", str.c_str());
-
-        str = str + " ";
-
-        this->clear();
-        size_t pos = 0;
-        std::string word;
-        std::string delimiter = " ";
-        while ((pos = str.find(delimiter)) != std::string::npos)
-        {
-          word = str.substr(0, pos);
-          str.erase(0, pos + delimiter.length());
-
-          int rest_of_line = this->line_length - (this->cursor_position % this->line_length);
-
-          if (rest_of_line < word.length())
-          {
-            if (!this->_next_line())
-            {
-              break;
-            }
-          }
-
-          bool nl = false;
-          if ((pos = word.find('\n')) != std::string::npos)
-          {
-            if (pos == 0)
-            {
-              this->_next_line();
-              nl = false;
-            }
-            else
-              nl = true;
-            word.erase(pos, 1);
-          }
-
-          this->_write_string(word);
-
-          if (nl)
-            this->_next_line();
-          else
-            this->_cursor_increment();
-        }
-
-        this->adapt();
-      }
-
-      void testpattern()
-      {
-        this->clear();
-        for (int i = 0; i < this->MAX_ADDRESS; i++)
-        {
-          this->_write_char(48 + ((i + 1) % 10));
-        }
-        this->adapt();
-      }
+      void set_row(std::string str, uint8_t mode = 0x00);
     };
-
   }
 }
