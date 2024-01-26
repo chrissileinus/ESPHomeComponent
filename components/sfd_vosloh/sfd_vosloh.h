84f76557c71a2635cc29d1ea466dd05291e48355
Christian Backus <chrissileinus@googlemail.com>
2024-01-26 13:40:12 +0100

    Update sfd_vosloh.h

--- components/sfd_vosloh/sfd_vosloh.h
+++ components/sfd_vosloh/sfd_vosloh.h
@@ -5,6 +5,7 @@
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome
 {
@@ -19,11 +20,11 @@ namespace esphome
     static const uint8_t WORD_WRAP = 0b00010000;
     static const uint8_t OVERWRITE = 0b00001000;
 
-    class sfdVosloh : public Component, public uart::UARTDevice, public text_sensor::TextSensor, public sensor::Sensor
+    class sfdVosloh : public Component, public uart::UARTDevice, public text_sensor::TextSensor, public sensor::Sensor, public binary_sensor::BinarySensor
     {
     protected:
       const uint8_t POSITION_MAX = 127;
-      const uint8_t UART_TIMEOUT = 10;
+      const uint8_t UART_TIMEOUT = 20;
 
       const uint8_t _WRITE = 0x88;
       const uint8_t _READ = 0x85;
@@ -34,11 +35,13 @@ namespace esphome
       int row_length = this->POSITION_MAX;
       int current_position = 1;
       int blocked = 0;
+      int loop_counter = 0;
 
       text_sensor::TextSensor *current_content;
       text_sensor::TextSensor *current_c_state;
       text_sensor::TextSensor *current_m_state;
       sensor::Sensor *last_module;
+      binary_sensor::BinarySensor *rolling;
 
       bool set_string(std::string string, uint8_t position);
       bool set_character(char character, uint8_t position);
@@ -48,6 +51,7 @@ namespace esphome
       uint8_t collect_respond();
 
       void update_last_module();
+      void update_current_content();
       void update_current_state();
 
       // helper
@@ -69,12 +73,14 @@ namespace esphome
       void setup_current_c_state(text_sensor::TextSensor *sensor) { this->current_c_state = sensor; }
       void setup_current_m_state(text_sensor::TextSensor *sensor) { this->current_m_state = sensor; }
       void setup_last_module(sensor::Sensor *sensor) { this->last_module = sensor; }
+      void setup_rolling(binary_sensor::BinarySensor *sensor) { this->rolling = sensor; }
 
       void setup() override;
       void dump_config() override;
       void loop() override;
 
       void roll();
+      void adapt();
       void clear(bool adapt = true);
 
       void set_content(std::string str, uint8_t mode = 0x00, uint8_t row = 1);
